be0a828fd9bf07f1f253e02a5858b225d56d9a2f
Webber Chagas <webberchagas@gmail.com>
2025-05-11 21:23:18 -0300

    First commit

---
+++ src/main/java/br/com/fiap/foodtech/FoodtechApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.foodtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodtechApplication.class, args);
+	}
+
+}
